b1dc120165aeb8bef7d73c0aeb814d6e11d01a57
bebroch <cawapog@yandex.ru>
2024-03-13 10:22:56 +0300

    init: project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div style={{ textAlign: "center" }}>
+                <h1>{"Прив)"}</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
