0565044048d9492bbe591dac5b68cb5897e38856
bebroch <cawapog@yandex.ru>
2024-03-14 20:34:59 +0300

    refactor.server: add orm config

---
+++ server/database/typeOrmConfig.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+import { Status } from "./entity/status.entity"
+import { Tag } from "./entity/tag.entity"
+import { Task } from "./entity/task.entity"
+import { User } from "./entity/user.entity"
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "password",
+    database: "todo_list_database",
+    entities: [Task, User, Status, Tag],
+    synchronize: true,
+    autoLoadEntities: true,
+}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,28 +1,12 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
-import { Status } from "database/entity/status.entity"
-import { Tag } from "database/entity/tag.entity"
-import { Task } from "database/entity/task.entity"
-import { User } from "database/entity/user.entity"
+import { typeOrmConfig } from "../database/typeOrmConfig"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { TaskModule } from "./task/task.module"
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "password",
-            database: "todo_list_database",
-            entities: [Task, User, Status, Tag],
-            synchronize: false,
-            autoLoadEntities: false,
-        }),
-        TaskModule,
-    ],
+    imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule],
     controllers: [AppController],
     providers: [AppService],
 })
