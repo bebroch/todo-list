865ac2aece9011123bba80bbb3c9c5438f11a9a5
bebroch <cawapog@yandex.ru>
2024-03-17 17:00:49 +0300

    feat: make mini test

--- server/libs/database/src/entity/task/task.service.spec.ts
+++ server/libs/database/src/entity/task/task.service.spec.ts
@@ -1,15 +1,14 @@
+import { Task } from "@database-config/entity/task.entity"
 import { Test, TestingModule } from "@nestjs/testing"
-import { getRepositoryToken } from "@nestjs/typeorm"
-import { Repository } from "typeorm"
-import { Task } from "../../../../../database/entity/task.entity"
 import { CreateTagDto } from "../tag/dto/create-tag.dto"
 import { TagService } from "../tag/tag.service"
 import { CreateTaskDto } from "./dto/create-task.dto"
+import { TaskDatabaseService } from "./task-database/task-database.service"
 import { TaskService } from "./task.service"
 
 describe("TaskService", () => {
     let service: TaskService
-    let repositoryMock: Repository<Task>
+    let taskDatabaseService: TaskDatabaseService
     const mockTasks: Task[] = [
         {
             id: 1,
@@ -74,20 +73,24 @@ describe("TaskService", () => {
             providers: [
                 {
                     // FIXME поправить тест
-                    provide: getRepositoryToken(Task),
+                    provide: TaskDatabaseService,
                     useValue: {
-                        find: jest.fn(),
-                        findOne: jest.fn(),
+                        findAll: jest.fn(),
+                        findMany: jest.fn(),
                         create: jest.fn(),
+                        updateMany: jest.fn(),
                         update: jest.fn(),
-                        delete: jest.fn(),
+                        removeMany: jest.fn(),
+                        removeOne: jest.fn(),
                         save: jest.fn(),
+                        getQueryBuilder: jest.fn().mockReturnThis(),
                     },
                 },
                 {
                     provide: TagService,
                     useValue: {
                         createOrFindMany: jest.fn(),
+                        findManyByNames: jest.fn().mockResolvedValue([]),
                     },
                 },
                 TaskService,
@@ -95,7 +98,7 @@ describe("TaskService", () => {
         }).compile()
 
         service = module.get<TaskService>(TaskService)
-        repositoryMock = module.get<Repository<Task>>(getRepositoryToken(Task))
+        taskDatabaseService = module.get<TaskDatabaseService>(TaskDatabaseService)
     })
 
     it("should be defined", () => {
@@ -103,18 +106,19 @@ describe("TaskService", () => {
     })
 
     it("find all", async () => {
-        jest.spyOn(repositoryMock, "find").mockResolvedValue(mockTasks)
+        // @ts-ignore
+        jest.spyOn(taskDatabaseService, "findAll").mockResolvedValue(mockTasks)
 
         const tasks = await service.findAll()
 
         expect(tasks).toEqual(mockTasks)
     })
 
-    it("create", async () => {
+    it("test create", async () => {
         const createdTask = { id: 20, title: "asdsad", description: "asdsadsa", status: "asdasd" }
 
         // @ts-ignore
-        jest.spyOn(repositoryMock, "create").mockResolvedValue(createdTask)
+        jest.spyOn(taskDatabaseService, "create").mockResolvedValue(createdTask)
 
         const tasks = await service.create(
             new CreateTaskDto({
@@ -127,4 +131,32 @@ describe("TaskService", () => {
 
         expect(tasks).toEqual(createdTask)
     })
+
+    it.skip("test find many", async () => {
+        const findConfig = {
+            page: 1,
+            limit: 10,
+            query: "",
+            statuses: ["asdsad", "fgfdg"],
+            tags: ["dfbg", "dfg"],
+        }
+
+        await service.findMany(findConfig)
+
+        expect(taskDatabaseService.getQueryBuilder).toHaveBeenCalledTimes(findConfig.page)
+        expect(taskDatabaseService.getQueryBuilder().skip).toHaveBeenCalledWith(
+            (findConfig.page - 1) * findConfig.limit,
+        )
+        expect(taskDatabaseService.getQueryBuilder().take).toHaveBeenCalledWith(findConfig.limit)
+        expect(taskDatabaseService.getQueryBuilder().andWhere).toHaveBeenCalledWith(
+            "(task.title LIKE :query OR task.description LIKE :query)",
+            { query: `%${findConfig.query}%` },
+        )
+        expect(taskDatabaseService.getQueryBuilder().andWhere).toHaveBeenCalledWith(
+            "task.status IN (:...statuses)",
+            { statuses: findConfig.statuses },
+        )
+    })
+
+    it.skip("test update", async () => {})
 })
