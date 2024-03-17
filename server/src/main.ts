879a917e738d6de39d598331ced914a186286a16
bebroch <cawapog@yandex.ru>
2024-03-17 21:34:25 +0300

    feat.server: .env

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,18 @@
+import * as dotenv from "dotenv"
+import { resolve } from "path"
+dotenv.config({ path: resolve(__dirname, "..", "..", ".env") })
+
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 
-const PORT = 5000
-
 async function bootstrap() {
+    const PORT = process.env.PORT || 5000
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(new ValidationPipe({ transform: true, forbidUnknownValues: true }))
     await app.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`)
     })
 }
+
 bootstrap()
